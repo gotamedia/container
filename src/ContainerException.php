5e461acf8ba29c6fed65b9f9d45d26989750cc0f
Peter Pettersson <peter.pettersson@gotamedia.se>
2019-03-14 10:35:24 +0100

    Fixed exceptions to be actual exceptions

--- src/ContainerException.php
+++ src/ContainerException.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Atoms\Container;
 
+use Exception;
 use Psr\Container\ContainerExceptionInterface;
 
-class ContainerException implements ContainerExceptionInterface
+class ContainerException extends Exception implements ContainerExceptionInterface
 {
 }
